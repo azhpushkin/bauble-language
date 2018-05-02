e8d983026f0f8fa69ed6570dde2316001c7bf68a
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2018-05-02 22:22:44 +0300

    Add break and continue

---
+++ src/Syntax.hs
@@ -0,0 +1,51 @@
+module Syntax where
+
+import qualified Data.Map as Map
+
+
+data Value = Integer Integer
+           | Rational (Integer, Integer)  -- Cannot be directly created
+           | Double Double
+           | Boolean Bool
+           | Closure (Map.Map String Value) [String] [Expr]  -- Closure is not created directly
+           | Null
+           deriving (Show, Ord, Eq)
+
+
+
+data Expr = Value Value
+          | Variable String
+          | Assign String Expr
+
+          | Call Expr [Expr]
+          | Function [String] [Expr]  -- Defining a function
+          | Return Expr  -- Only inside of Function
+
+          | BinOperator BinOperator Expr Expr
+          | UnOperator UnOperator Expr
+          | If Expr [Expr] (Maybe [Expr])
+
+          | While Expr [Expr]
+          | Continue  -- Only inside of While
+          | Break  -- Only inside of While
+          deriving (Show, Ord, Eq)
+
+data BinOperator = Plus
+                 | Minus
+                 | Multiply
+                 | Divide
+
+                 | Less
+                 | LessOrEqual
+                 | Greater
+                 | GreaterOrEqual
+                 | Equal
+                 | NotEqual
+
+                 | And
+                 | Or
+                 deriving (Show, Ord, Eq)
+
+data UnOperator = Not
+                | Negate
+                deriving (Show, Ord, Eq)
