8150d7a22548b69849a392d81aa4c3bb403c5bb2
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2018-05-30 01:42:37 +0300

    Add Builtins.hs implementation

--- src/Builtins.hs
+++ src/Builtins.hs
@@ -2,6 +2,44 @@ module Builtins where
 
 
 import Syntax
+import Data.List
+
+valueToString :: Value -> String
+valueToString value =
+  case value of
+    Integer i -> (show i)
+    Double d  -> (show d)
+    Rational r  -> (show r)
+
+    Boolean True -> "true"
+    Boolean False -> "false"
+
+    String s -> s
+
+    Null -> "null"
+
+    Closure (Just name) _ args _ -> ("<Closure \"" ++ name++ "\" with args " ++ show args ++ ">")
+    Closure Nothing     _ args _ -> ("<Closure with args " ++ show args ++ ">")
+
+    BuiltinFunction Print -> "<Built-in function print>"
+    BuiltinFunction IsNull -> "<Built-in function isnull>"
+
+    Array values -> ("[" ++ intercalate ", " (map valueToString values) ++ "]")
 
 proceedBuiltin :: BuiltinFunction -> [Value] -> IO (Value)
-proceedBuiltin = error "asd"
+
+proceedBuiltin Print [] = putStrLn "" >> return Null
+proceedBuiltin Print (value:rest) = do
+  putStr $ (valueToString value)
+  proceedBuiltin Print rest
+
+
+proceedBuiltin IsNull [v] =
+  case v of
+    Null -> return (Boolean True)
+    _    -> return (Boolean True)
+proceedBuiltin IsNull args =
+  error $ "isnull expected single argument, but got " ++ show args ++ "!"
+
+
+
